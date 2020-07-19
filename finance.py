2d1bf78964d6f428f22e137155f60068f479a831
Ahmed Aly <ahmed@umbragestudios.com>
2020-07-19 08:52:23 -0500

    feat(plot volume): plot closing price, 100ma, and volume for amzn ticker

--- finance.py
+++ finance.py
@@ -10,12 +10,17 @@ style.use('ggplot')
 start = dt.datetime(2000, 1, 1)
 end = dt.datetime(2016, 12, 31)
 
-
 df = web.DataReader('AMZN', 'yahoo', start)
 df.to_csv('amzn.csv')
 
 df = pd.read_csv('amzn.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100).mean()
-df.dropna(inplace=True)
-df['100ma'].plot()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.plot(df.index, df['Volume'])
+
 plt.show()
