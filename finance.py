66a85771c2f9c207139c4151e256f439e7b0401f
Ahmed Aly <ahmed@umbragestudios.com>
2020-07-19 14:11:56 -0500

    feat(sp500): pull data for all companies and combine into one dataset

--- finance.py
+++ finance.py
@@ -1,35 +1,7 @@
-import datetime as dt
-
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-import pandas as pd
-import pandas_datareader.data as web
 from matplotlib import style
-from mplfinance.original_flavor import candlestick_ohlc
+
+from services.sp500 import compile_data
 
 style.use('ggplot')
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
-
-df = web.DataReader('AMZN', 'yahoo', start)
-df.to_csv('amzn.csv')
-
-df = pd.read_csv('amzn.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_vol = df['Volume'].resample('10D').sum()
-
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-
-ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
-ax1.xaxis_date()
-
-candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
-
-ax2.fill_between(df_vol.index.map(mdates.date2num), df_vol.values, 0)
-
-plt.show()
+compile_data()
---
+++ services/sp500.py
@@ -0,0 +1,68 @@
+import datetime as dt
+import os
+import pickle
+
+import bs4 as bs
+import pandas as pd
+import pandas_datareader.data as web
+import requests
+
+
+def save_sp500_tickers():
+    response = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(response.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.find_all('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker.replace(".", "-").strip())
+    with open("sp500.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+
+    return tickers
+
+
+def get_ticker_data(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2020, 7, 15)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+
+def compile_data():
+    with open("sp500.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    main_df.to_csv('stock_dfs/sp500_joined_closes.csv')
